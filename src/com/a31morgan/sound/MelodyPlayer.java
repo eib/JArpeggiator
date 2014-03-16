906347c125a13f3c06a2f43559ac9e6e600251d7
A31Morgan <morgan.blackwelder@gmail.com>
2014-03-16 13:09:41 -0400

    Added Stockholm Syndrome Arpeggios 1.0

--- src/com/a31morgan/sound/MelodyPlayer.java
+++ src/com/a31morgan/sound/MelodyPlayer.java
@@ -40,25 +40,35 @@ public class MelodyPlayer {
     public static void main(String[] args) throws LineUnavailableException {
         Melody melody = new Melody(129);
         for (int ii = 0; ii < 1; ii++) {
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.C4, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.A3$, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.F3, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.C4, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.A3$, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.F3, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.C4, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.C4, 3), SIXTEENTH_NOTE);
-	        //Chorus Progression:
-	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.C4, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.A3, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(new Arpeggio(new int[] {5, 4, 3}), Pitch.G3, 3), SIXTEENTH_NOTE); //C Major Chord 2nd Inversion, G (+5) C (+4) E (+3)
-	        melody.addAll(UP_DOWN.getArpeggio(new Arpeggio(new int[] {5, 4, 3}), Pitch.G3, 3), SIXTEENTH_NOTE); //C Major Chord 2nd Inversion, G (+5) C (+4) E (+3)
-	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.C4, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.A3, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.E3, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.E3, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.F3, 3), SIXTEENTH_NOTE);
-	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.F3, 3), SIXTEENTH_NOTE);	        
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.C4, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.A3$, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.F3, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.C4, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.A3$, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.F3, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.C4, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.C4, 3), SIXTEENTH_NOTE);
+//	        //Chorus Progression:
+//	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.C4, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.A3, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(new Arpeggio(new int[] {5, 4, 3}), Pitch.G3, 3), SIXTEENTH_NOTE); //C Major Chord 2nd Inversion, G (+5) C (+4) E (+3)
+//	        melody.addAll(UP_DOWN.getArpeggio(new Arpeggio(new int[] {5, 4, 3}), Pitch.G3, 3), SIXTEENTH_NOTE); //C Major Chord 2nd Inversion, G (+5) C (+4) E (+3)
+//	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.C4, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.A3, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.E3, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MAJOR, Pitch.E3, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.F3, 3), SIXTEENTH_NOTE);
+//	        melody.addAll(UP_DOWN.getArpeggio(MINOR, Pitch.F3, 3), SIXTEENTH_NOTE);
+	        
+	        
+	        //Stockholm Syndrome Chorus Progression:															
+	        melody.addAll(UP.getArpeggio(new Arpeggio(new int[] {4, 3, 5, -8,}), Pitch.F3, 3), SIXTEENTH_NOTE); //These are going to be tricky, it goes back to the second note of the arpeggio
+	        melody.addAll(UP.getArpeggio(new Arpeggio(new int[] {4, 3, 5, -8,}), Pitch.E3, 3), SIXTEENTH_NOTE); //A Major Chord 2nd Inversion, E (+5) A (+4) C$(+3)
+	        melody.addAll(DOWN.getArpeggio(MINOR, Pitch.D7, 3), SIXTEENTH_NOTE); 
+	        melody.addAll(DOWN.getArpeggio(MINOR, Pitch.D6, 3), SIXTEENTH_NOTE);
+	        //http://takkaria.org/sheets/muse-ss-chorus1.pdf
+	        //http://www.youtube.com/watch?v=GaNYUtoEevo   you're welcome ;)
+	        
         }
         melody.addAll(new Note(Pitch.REST, WHOLE_NOTE).times(3));
         
