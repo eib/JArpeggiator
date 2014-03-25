7f46e6773c8faf7da1013c8b9406fa361fae08fb
eib <eib@github>
2014-03-24 22:10:01 -0400

    Adding scale/fade filters to Arpeggiator Main.

--- src/com/a31morgan/sound/gui/ArpeggiatorMain.java
+++ src/com/a31morgan/sound/gui/ArpeggiatorMain.java
@@ -10,6 +10,9 @@ import javax.swing.WindowConstants;
 
 import com.a31morgan.sound.Pitch;
 import com.a31morgan.sound.player.BackgroundPlayer;
+import com.a31morgan.sound.player.EchoFilter;
+import com.a31morgan.sound.player.Filter;
+import com.a31morgan.sound.player.IFilter;
 import com.a31morgan.sound.player.IPlayer;
 import com.a31morgan.sound.player.Player;
 
@@ -23,7 +26,18 @@ public class ArpeggiatorMain {
 
 	public static void main(String[] args) throws Exception {
     	AudioFormat format = new AudioFormat(Pitch.SAMPLE_RATE, 8, 1, true, true);
-    	final IPlayer player = new BackgroundPlayer(new Player(format));
+    	
+    	IFilter[] filters = {
+    			Filter.createScaleFilter(0.6),
+    			Filter.createFadeInOutFilter(20, Pitch.SAMPLE_RATE),
+        		EchoFilter.createMultiEchoFilter(new double[] { //in fractions
+            			0.5, 0.4, 0.2, 0.1, 0.05,
+            		}, new int[] { //frames
+            			1500, 3000, 8000, 12000, 14000,
+            		}),
+    	};
+    	
+    	final IPlayer player = new BackgroundPlayer(new Player(format, filters));
     	
 		JFrame frame = new JFrame("JArpeggiator");
 		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
